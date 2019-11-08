caba67e0f46ee931e16b1d55ce2b8cc3619173cc
Natalia Portillo <claunia@claunia.com>
2019-11-08 00:23:14 +0000

    Set layout for CRUD pages.

--- DiscImageChef.Server/Areas/Admin/Controllers/HomeController.cs
+++ DiscImageChef.Server/Areas/Admin/Controllers/HomeController.cs
@@ -11,7 +11,7 @@
 //
 //     Provides documentation data for razor views.
 //
-// --[ License ] --------------------------------------------------------------
+    // --[ License ] --------------------------------------------------------------
 //
 //     This library is free software; you can redistribute it and/or modify
 //     it under the terms of the GNU Lesser General Public License as
