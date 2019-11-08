86ae22c42f83dc1626bbf78a2395c98b09ad439f
Natalia Portillo <claunia@claunia.com>
2019-11-08 01:16:20 +0000

    Do not allow editing ATA IDENTIFY responses.

--- DiscImageChef.Server/Areas/Admin/Controllers/AtasController.cs
+++ DiscImageChef.Server/Areas/Admin/Controllers/AtasController.cs
@@ -67,57 +67,6 @@ namespace DiscImageChef.Server.Areas.Admin.Controllers
             return View(ata);
         }
 
-        // GET: Admin/Atas/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var ata = await _context.Ata.FindAsync(id);
-            if (ata == null)
-            {
-                return NotFound();
-            }
-            return View(ata);
-        }
-
-        // POST: Admin/Atas/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Identify")] DiscImageChef.CommonTypes.Metadata.Ata ata)
-        {
-            if (id != ata.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(ata);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AtaExists(ata.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(ata);
-        }
-
         // GET: Admin/Atas/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
