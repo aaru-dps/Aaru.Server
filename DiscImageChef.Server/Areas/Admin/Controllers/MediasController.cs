270d945e27b5494913c7afed4734f4f5d4b3002f
Natalia Portillo <claunia@claunia.com>
2019-11-11 00:25:05 +0000

    Allow to independently show real, image, or all media types.

--- DiscImageChef.Server/Areas/Admin/Controllers/MediasController.cs
+++ DiscImageChef.Server/Areas/Admin/Controllers/MediasController.cs
@@ -15,8 +15,18 @@ namespace DiscImageChef.Server.Areas.Admin.Controllers
         public MediasController(DicServerContext context) => _context = context;
 
         // GET: Admin/Medias
-        public IActionResult Index() => View(_context.Medias.ToList().OrderBy(m => m.PhysicalType).
-                                                      ThenBy(m => m.LogicalType).ThenBy(m => m.Real));
+        public IActionResult Index(bool? real)
+        {
+            switch(real)
+            {
+                case null:
+                    return View(_context.Medias.ToList().OrderBy(m => m.PhysicalType).ThenBy(m => m.LogicalType).
+                                         ThenBy(m => m.Real));
+                default:
+                    return View(_context.Medias.Where(m => m.Real == real).ToList().OrderBy(m => m.PhysicalType).
+                                         ThenBy(m => m.LogicalType));
+            }
+        }
 
         // GET: Admin/Medias/Delete/5
         public async Task<IActionResult> Delete(int? id)
