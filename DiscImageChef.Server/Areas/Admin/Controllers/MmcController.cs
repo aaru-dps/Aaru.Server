30a8776016e458d80ca708f26223bf66eb6fd018
Natalia Portillo <claunia@claunia.com>
2019-11-12 23:57:11 +0000

    Remove edit from MMCs.

--- DiscImageChef.Server/Areas/Admin/Controllers/MmcController.cs
+++ DiscImageChef.Server/Areas/Admin/Controllers/MmcController.cs
@@ -36,58 +36,6 @@ namespace DiscImageChef.Server.Areas.Admin.Controllers
             return View(mmc);
         }
 
-        // GET: Admin/Mmc/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if(id == null)
-            {
-                return NotFound();
-            }
-
-            Mmc mmc = await _context.Mmc.FindAsync(id);
-
-            if(mmc == null)
-            {
-                return NotFound();
-            }
-
-            return View(mmc);
-        }
-
-        // POST: Admin/Mmc/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost, ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ModeSense2AData")] Mmc mmc)
-        {
-            if(id != mmc.Id)
-            {
-                return NotFound();
-            }
-
-            if(ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(mmc);
-                    await _context.SaveChangesAsync();
-                }
-                catch(DbUpdateConcurrencyException)
-                {
-                    if(!MmcExists(mmc.Id))
-                    {
-                        return NotFound();
-                    }
-
-                    throw;
-                }
-
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(mmc);
-        }
-
         // GET: Admin/Mmc/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -116,7 +64,5 @@ namespace DiscImageChef.Server.Areas.Admin.Controllers
 
             return RedirectToAction(nameof(Index));
         }
-
-        bool MmcExists(int id) => _context.Mmc.Any(e => e.Id == id);
     }
 }
