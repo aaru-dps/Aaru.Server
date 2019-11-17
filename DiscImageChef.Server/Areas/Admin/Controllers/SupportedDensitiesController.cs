991ffb3f09b0b138de6fdb4419fe1324eb8e3cc4
Natalia Portillo <claunia@claunia.com>
2019-11-17 20:52:41 +0000

    Remove edit and details from supported densities.

--- DiscImageChef.Server/Areas/Admin/Controllers/SupportedDensitiesController.cs
+++ DiscImageChef.Server/Areas/Admin/Controllers/SupportedDensitiesController.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using System.Threading.Tasks;
 using DiscImageChef.CommonTypes.Metadata;
 using DiscImageChef.Server.Models;
@@ -17,79 +16,6 @@ namespace DiscImageChef.Server.Areas.Admin.Controllers
 
         // GET: Admin/SupportedDensities
         public async Task<IActionResult> Index() => View(await _context.SupportedDensity.ToListAsync());
-
-        // GET: Admin/SupportedDensities/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if(id == null)
-            {
-                return NotFound();
-            }
-
-            SupportedDensity supportedDensity = await _context.SupportedDensity.FirstOrDefaultAsync(m => m.Id == id);
-
-            if(supportedDensity == null)
-            {
-                return NotFound();
-            }
-
-            return View(supportedDensity);
-        }
-
-        // GET: Admin/SupportedDensities/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if(id == null)
-            {
-                return NotFound();
-            }
-
-            SupportedDensity supportedDensity = await _context.SupportedDensity.FindAsync(id);
-
-            if(supportedDensity == null)
-            {
-                return NotFound();
-            }
-
-            return View(supportedDensity);
-        }
-
-        // POST: Admin/SupportedDensities/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost, ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(
-            int id, [Bind(
-                "Id,PrimaryCode,SecondaryCode,Writable,Duplicate,DefaultDensity,BitsPerMm,Width,Tracks,Capacity,Organization,Name,Description")]
-            SupportedDensity supportedDensity)
-        {
-            if(id != supportedDensity.Id)
-            {
-                return NotFound();
-            }
-
-            if(ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(supportedDensity);
-                    await _context.SaveChangesAsync();
-                }
-                catch(DbUpdateConcurrencyException)
-                {
-                    if(!SupportedDensityExists(supportedDensity.Id))
-                    {
-                        return NotFound();
-                    }
-
-                    throw;
-                }
-
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(supportedDensity);
-        }
 
         // GET: Admin/SupportedDensities/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -119,7 +45,5 @@ namespace DiscImageChef.Server.Areas.Admin.Controllers
 
             return RedirectToAction(nameof(Index));
         }
-
-        bool SupportedDensityExists(int id) => _context.SupportedDensity.Any(e => e.Id == id);
     }
 }
