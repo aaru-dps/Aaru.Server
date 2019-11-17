cc7fe43821c44ebbb64658f609151a7678dd5830
Natalia Portillo <claunia@claunia.com>
2019-11-17 20:23:32 +0000

    Sort SSCs.

--- DiscImageChef.Server/Areas/Admin/Controllers/SscsController.cs
+++ DiscImageChef.Server/Areas/Admin/Controllers/SscsController.cs
@@ -16,7 +16,9 @@ namespace DiscImageChef.Server.Areas.Admin.Controllers
         public SscsController(DicServerContext context) => _context = context;
 
         // GET: Admin/Sscs
-        public async Task<IActionResult> Index() => View(await _context.Ssc.ToListAsync());
+        public async Task<IActionResult> Index() =>
+            View(await _context.Ssc.OrderBy(s => s.MinBlockLength).ThenBy(s => s.MaxBlockLength).
+                                ThenBy(s => s.BlockSizeGranularity).ToListAsync());
 
         // GET: Admin/Sscs/Details/5
         public async Task<IActionResult> Details(int? id)
