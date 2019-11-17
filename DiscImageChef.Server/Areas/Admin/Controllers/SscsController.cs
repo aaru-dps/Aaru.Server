53b8163a43252b9c3a810f4ec01937e4ac15d89d
Natalia Portillo <claunia@claunia.com>
2019-11-17 20:46:37 +0000

    Add consolidation for SSCs.

--- DiscImageChef.Server/Areas/Admin/Controllers/SscsController.cs
+++ DiscImageChef.Server/Areas/Admin/Controllers/SscsController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DiscImageChef.CommonTypes.Metadata;
@@ -5,6 +6,7 @@ using DiscImageChef.Server.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace DiscImageChef.Server.Areas.Admin.Controllers
 {
@@ -45,6 +47,70 @@ namespace DiscImageChef.Server.Areas.Admin.Controllers
             Ssc ssc = await _context.Ssc.FindAsync(id);
             _context.Ssc.Remove(ssc);
             await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Consolidate()
+        {
+            List<SscModel> dups = _context.Ssc.GroupBy(x => new
+            {
+                x.BlockSizeGranularity, x.MaxBlockLength, x.MinBlockLength
+            }).Where(x => x.Count() > 1).Select(x => new SscModel
+            {
+                BlockSizeGranularity = x.Key.BlockSizeGranularity, MaxBlockLength = x.Key.MaxBlockLength,
+                MinBlockLength       = x.Key.MinBlockLength
+            }).ToList();
+
+            return View(new SscModelForView
+            {
+                List = dups, Json = JsonConvert.SerializeObject(dups)
+            });
+        }
+
+        [HttpPost, ActionName("Consolidate"), ValidateAntiForgeryToken]
+        public IActionResult ConsolidateConfirmed(string models)
+        {
+            SscModel[] duplicates;
+
+            try
+            {
+                duplicates = JsonConvert.DeserializeObject<SscModel[]>(models);
+            }
+            catch(JsonSerializationException)
+            {
+                return BadRequest();
+            }
+
+            if(duplicates is null)
+                return BadRequest();
+
+            foreach(SscModel duplicate in duplicates)
+            {
+                Ssc master =
+                    _context.Ssc.FirstOrDefault(m => m.BlockSizeGranularity == duplicate.BlockSizeGranularity &&
+                                                     m.MaxBlockLength       == duplicate.MaxBlockLength       &&
+                                                     m.MinBlockLength       == duplicate.MinBlockLength);
+
+                if(master is null)
+                    continue;
+
+                foreach(Ssc ssc in _context.Ssc.Where(m => m.BlockSizeGranularity == duplicate.BlockSizeGranularity &&
+                                                           m.MaxBlockLength       == duplicate.MaxBlockLength       &&
+                                                           m.MinBlockLength       == duplicate.MinBlockLength).Skip(1).
+                                            ToArray())
+                {
+                    foreach(TestedSequentialMedia media in _context.TestedSequentialMedia.Where(d => d.SscId == ssc.Id))
+                    {
+                        media.SscId = master.Id;
+                    }
+
+                    _context.Ssc.Update(ssc);
+                    _context.Ssc.Remove(ssc);
+                }
+            }
+
+            _context.SaveChanges();
 
             return RedirectToAction(nameof(Index));
         }
---
+++ DiscImageChef.Server/Models/SscModel.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace DiscImageChef.Server.Models
+{
+    public class SscModel
+    {
+        public byte? BlockSizeGranularity { get; set; }
+        public uint? MaxBlockLength       { get; set; }
+        public uint? MinBlockLength       { get; set; }
+    }
+
+    public class SscModelForView
+    {
+        public List<SscModel> List { get; set; }
+        public string         Json { get; set; }
+    }
+}
