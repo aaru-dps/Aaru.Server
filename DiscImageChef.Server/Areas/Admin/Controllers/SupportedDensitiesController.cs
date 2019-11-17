e63d7daaa095c404f6ef06941fd3f3198bdfb9e8
Natalia Portillo <claunia@claunia.com>
2019-11-17 21:12:26 +0000

    Sort supported densities.

--- DiscImageChef.Server/Areas/Admin/Controllers/SupportedDensitiesController.cs
+++ DiscImageChef.Server/Areas/Admin/Controllers/SupportedDensitiesController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using DiscImageChef.CommonTypes.Metadata;
 using DiscImageChef.Server.Models;
@@ -15,7 +16,14 @@ namespace DiscImageChef.Server.Areas.Admin.Controllers
         public SupportedDensitiesController(DicServerContext context) => _context = context;
 
         // GET: Admin/SupportedDensities
-        public async Task<IActionResult> Index() => View(await _context.SupportedDensity.ToListAsync());
+        public async Task<IActionResult> Index() => View(await _context.
+                                                               SupportedDensity.OrderBy(d => d.Organization).
+                                                               ThenBy(d => d.Name).ThenBy(d => d.Description).
+                                                               ThenBy(d => d.Capacity).ThenBy(d => d.PrimaryCode).
+                                                               ThenBy(d => d.SecondaryCode).ThenBy(d => d.BitsPerMm).
+                                                               ThenBy(d => d.Width).ThenBy(d => d.Tracks).
+                                                               ThenBy(d => d.DefaultDensity).ThenBy(d => d.Writable).
+                                                               ThenBy(d => d.Duplicate).ToListAsync());
 
         // GET: Admin/SupportedDensities/Delete/5
         public async Task<IActionResult> Delete(int? id)
