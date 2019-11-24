2cbc92e1180525a30936411bbb27a7d84c67c7fe
Natalia Portillo <claunia@claunia.com>
2019-11-24 00:06:41 +0000

    Show tested sequential media in uploaded report details.

--- DiscImageChef.Server/Models/UploadedReportDetails.cs
+++ DiscImageChef.Server/Models/UploadedReportDetails.cs
@@ -5,12 +5,13 @@ namespace DiscImageChef.Server.Models
 {
     public class UploadedReportDetails
     {
-        public UploadedReport    Report                { get; set; }
-        public List<int>         SameAll               { get; set; }
-        public List<int>         SameButManufacturer   { get; set; }
-        public List<int>         ReportAll             { get; set; }
-        public List<int>         ReportButManufacturer { get; set; }
-        public int               ReadCapabilitiesId    { get; set; }
-        public List<TestedMedia> TestedMedias          { get; set; }
+        public UploadedReport              Report                 { get; set; }
+        public List<int>                   SameAll                { get; set; }
+        public List<int>                   SameButManufacturer    { get; set; }
+        public List<int>                   ReportAll              { get; set; }
+        public List<int>                   ReportButManufacturer  { get; set; }
+        public int                         ReadCapabilitiesId     { get; set; }
+        public List<TestedMedia>           TestedMedias           { get; set; }
+        public List<TestedSequentialMedia> TestedSequentialMedias { get; set; }
     }
 }
