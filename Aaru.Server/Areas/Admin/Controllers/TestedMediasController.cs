9ba87460c233a2943d85a8d3a74972c8579dca4d
Rebecca Wallander <sakcheen@gmail.com>
2025-04-07 16:51:41 +0200

    Add raw DVD sector report parsing

--- Aaru.Server/Areas/Admin/Controllers/TestedMediasController.cs
+++ Aaru.Server/Areas/Admin/Controllers/TestedMediasController.cs
@@ -291,6 +291,10 @@ public sealed class TestedMediasController : Controller
                 buffer = testedMedia.HLDTSTReadRawDVDData;
 
                 break;
+            case nameof(testedMedia.LiteOnReadRawDVDData):
+                buffer = testedMedia.LiteOnReadRawDVDData;
+
+                break;
             case nameof(testedMedia.IdentifyData):
                 buffer        = testedMedia.IdentifyData;
                 model.Decoded = Identify.Prettify(buffer);
