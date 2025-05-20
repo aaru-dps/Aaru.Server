1184d1357ff1ea4a9825706d8f337bc0c30cdfa7
Natalia Portillo <claunia@claunia.com>
2025-05-20 21:39:51 +0100

    Migrate commands chart to Blazor.Bootstrap.

--- Aaru.Server/Components/Pages/Statistics/Commands.razor.cs
+++ Aaru.Server/Components/Pages/Statistics/Commands.razor.cs
@@ -1,5 +1,5 @@
 ﻿using Aaru.Server.Database.Models;
-using Blazorise.Charts;
+using BlazorBootstrap;
 using Microsoft.EntityFrameworkCore;
 using DbContext = Aaru.Server.Database.DbContext;
 
@@ -7,11 +7,11 @@ namespace Aaru.Server.Components.Pages.Statistics;
 
 public partial class Commands
 {
-    PieChart<long>? _commandsChart;
-    List<long>      _commandsCounts = [];
-    string[]        _commandsLabels = [];
-    bool            _isAlreadyInitialized;
-    List<Command>   CommandsList { get; set; } = [];
+    PieChart      _commandsChart;
+    List<double?> _commandsCounts = [];
+    List<string>  _commandsLabels = [];
+    bool          _isAlreadyInitialized;
+    List<Command> CommandsList { get; set; } = [];
 
     /// <inheritdoc />
     protected override async Task OnInitializedAsync()
@@ -35,31 +35,40 @@ public partial class Commands
         _commandsLabels = await ctx.Commands.OrderByDescending(static o => o.Count)
                                    .Take(10)
                                    .Select(static v => v.Name)
-                                   .ToArrayAsync();
+                                   .ToListAsync();
 
         _commandsCounts = await ctx.Commands.OrderByDescending(static o => o.Count)
                                    .Take(10)
-                                   .Select(static x => x.Count)
+                                   .Select(static x => (double?)x.Count)
                                    .ToListAsync();
 
-        if(_commandsLabels.Length >= 10)
+        if(_commandsLabels.Count >= 10)
         {
             _commandsLabels[9] = "Other";
 
             _commandsCounts[9] = ctx.Commands.Sum(static o => o.Count) - _commandsCounts.Take(9).Sum();
         }
 
-#pragma warning disable CS8604 // Possible null reference argument.
-        await Common.HandleRedrawAsync(_commandsChart, _commandsLabels, GetCommandsChartDataset);
-#pragma warning restore CS8604 // Possible null reference argument.
-    }
+        PieChartOptions pieChartOptions = new()
+        {
+            Responsive = true
+        };
 
-    PieChartDataset<long> GetCommandsChartDataset() => new()
-    {
-        Label           = $"Top {_commandsLabels.Length} used commands",
-        Data            = _commandsCounts,
-        BackgroundColor = Common.BackgroundColors,
-        BorderColor     = Common.BorderColors,
-        BorderWidth     = 1
-    };
+        pieChartOptions.Plugins.Title.Text    = $"Top {_commandsLabels.Count} used commands";
+        pieChartOptions.Plugins.Title.Display = true;
+
+        var chartData = new ChartData
+        {
+            Labels = _commandsLabels,
+            Datasets =
+            [
+                new PieChartDataset
+                {
+                    Data = _commandsCounts
+                }
+            ]
+        };
+
+        await _commandsChart.InitializeAsync(chartData, pieChartOptions);
+    }
 }
