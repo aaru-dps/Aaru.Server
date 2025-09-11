5389463cc07ae1f47936e72741bd11e1a6a2412d
Natalia Portillo <claunia@claunia.com>
2025-09-11 18:57:19 +0100

    Add delete button for versions page.

--- Aaru.Server/Components/Admin/Pages/Versions/View.razor.cs
+++ Aaru.Server/Components/Admin/Pages/Versions/View.razor.cs
@@ -1,3 +1,4 @@
+using BlazorBootstrap;
 using Microsoft.EntityFrameworkCore;
 using DbContext = Aaru.Server.Database.DbContext;
 using Version = Aaru.Server.Database.Models.Version;
@@ -6,8 +7,10 @@ namespace Aaru.Server.Components.Admin.Pages.Versions;
 
 public partial class View
 {
-    bool          _initialized;
-    List<Version> _items;
+    private int    _deleteId;
+    private Modal? _deleteModal;
+    bool           _initialized;
+    List<Version>  _items;
 
     /// <inheritdoc />
     protected override async Task OnInitializedAsync()
@@ -22,6 +25,43 @@ public partial class View
 
         _initialized = true;
 
+        StateHasChanged();
+    }
+
+    private async Task ShowDeleteModal(int id)
+    {
+        _deleteId = id;
+        if(_deleteModal != null) await _deleteModal.ShowAsync();
+    }
+
+    private async Task HideDeleteModal()
+    {
+        if(_deleteModal != null) await _deleteModal.HideAsync();
+    }
+
+    private async Task ConfirmDelete()
+    {
+        await DeleteVersionAsync(_deleteId);
+        await HideDeleteModal();
+        await RefreshItemsAsync();
+    }
+
+    private async Task DeleteVersionAsync(int id)
+    {
+        await using DbContext ctx     = await DbContextFactory.CreateDbContextAsync();
+        Version?              version = await ctx.Versions.FindAsync(id);
+
+        if(version is not null)
+        {
+            ctx.Versions.Remove(version);
+            await ctx.SaveChangesAsync();
+        }
+    }
+
+    private async Task RefreshItemsAsync()
+    {
+        await using DbContext ctx = await DbContextFactory.CreateDbContextAsync();
+        _items = await ctx.Versions.OrderBy(static v => v.Name).ToListAsync();
         StateHasChanged();
     }
 }
