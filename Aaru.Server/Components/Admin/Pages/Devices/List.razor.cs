356ff26944993f8e48c2f7fd6e24653e119a600f
Natalia Portillo <claunia@claunia.com>
2025-09-13 00:24:38 +0100

    Add device reports management page with delete functionality

---
+++ Aaru.Server/Components/Admin/Pages/Devices/List.razor.cs
@@ -0,0 +1,72 @@
+using Aaru.Server.Database.Models;
+using BlazorBootstrap;
+using Microsoft.EntityFrameworkCore;
+using DbContext = Aaru.Server.Database.DbContext;
+
+namespace Aaru.Server.Components.Admin.Pages.Devices;
+
+public partial class List
+{
+    private int    _deleteId;
+    private Modal? _deleteModal;
+    bool           _initialized;
+    List<Device>   _items;
+
+
+    /// <inheritdoc />
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        StateHasChanged();
+
+        await RefreshItemsAsync();
+
+        _initialized = true;
+
+        StateHasChanged();
+    }
+
+    async Task RefreshItemsAsync()
+    {
+        await using DbContext ctx = await DbContextFactory.CreateDbContextAsync();
+
+        _items = await ctx.Devices.OrderBy(static d => d.Manufacturer)
+                          .ThenBy(static d => d.Model)
+                          .ThenBy(static d => d.Revision)
+                          .ThenBy(static d => d.CompactFlash)
+                          .ThenBy(static d => d.Type)
+                          .ToListAsync();
+    }
+
+
+    private async Task ShowDeleteModal(int id)
+    {
+        _deleteId = id;
+        if(_deleteModal != null) await _deleteModal.ShowAsync();
+    }
+
+    private async Task HideDeleteModal()
+    {
+        if(_deleteModal != null) await _deleteModal.HideAsync();
+    }
+
+    private async Task ConfirmDelete()
+    {
+        await DeleteAsync(_deleteId);
+        await HideDeleteModal();
+        await RefreshItemsAsync();
+    }
+
+    private async Task DeleteAsync(int id)
+    {
+        await using DbContext ctx    = await DbContextFactory.CreateDbContextAsync();
+        Device?               device = await ctx.Devices.FindAsync(id);
+
+        if(device is not null)
+        {
+            ctx.Devices.Remove(device);
+            await ctx.SaveChangesAsync();
+        }
+    }
+}
