fb8f26d99e918440934646d1a54e55d56c3e97dd
Natalia Portillo <claunia@claunia.com>
2025-09-12 02:25:38 +0100

    Add view for MMC / SD list.

---
+++ Aaru.Server/Components/Admin/Pages/MmcSds/View.razor.cs
@@ -0,0 +1,67 @@
+using Aaru.CommonTypes.Metadata;
+using BlazorBootstrap;
+using Microsoft.EntityFrameworkCore;
+using DbContext = Aaru.Server.Database.DbContext;
+
+namespace Aaru.Server.Components.Admin.Pages.MmcSds;
+
+public partial class View
+{
+    private int    _deleteId;
+    private Modal? _deleteModal;
+    bool           _initialized;
+    List<MmcSd>    _items;
+
+    /// <inheritdoc />
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        StateHasChanged();
+
+        await using DbContext ctx = await DbContextFactory.CreateDbContextAsync();
+
+        _items = await ctx.MmcSd.ToListAsync();
+
+        _initialized = true;
+
+        StateHasChanged();
+    }
+
+    private async Task ShowDeleteModal(int id)
+    {
+        _deleteId = id;
+        if(_deleteModal != null) await _deleteModal.ShowAsync();
+    }
+
+    private async Task HideDeleteModal()
+    {
+        if(_deleteModal != null) await _deleteModal.HideAsync();
+    }
+
+    private async Task ConfirmDelete()
+    {
+        await DeleteVersionAsync(_deleteId);
+        await HideDeleteModal();
+        await RefreshItemsAsync();
+    }
+
+    private async Task DeleteVersionAsync(int id)
+    {
+        await using DbContext ctx   = await DbContextFactory.CreateDbContextAsync();
+        MmcSd?                mmcSd = await ctx.MmcSd.FindAsync(id);
+
+        if(mmcSd is not null)
+        {
+            ctx.MmcSd.Remove(mmcSd);
+            await ctx.SaveChangesAsync();
+        }
+    }
+
+    private async Task RefreshItemsAsync()
+    {
+        await using DbContext ctx = await DbContextFactory.CreateDbContextAsync();
+        _items = await ctx.MmcSd.ToListAsync();
+        StateHasChanged();
+    }
+}
