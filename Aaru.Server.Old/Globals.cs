3684326ab9323cb82f93fec3acb7ea2b9d5aa5e5
Natalia Portillo <claunia@claunia.com>
2025-09-13 05:25:31 +0100

    Remove old server files.

--- Aaru.Server.Old/Globals.cs
+++
@@ -1,7 +0,0 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using Microsoft.AspNetCore.Authorization;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.EntityFrameworkCore;
-global using System.Threading.Tasks;
