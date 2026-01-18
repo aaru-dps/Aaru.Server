4daf598bd8873e28b42b1fe09d0676f278e650dc
Rebecca Wallander <sakcheen@gamil.com>
2026-01-18 10:13:45 +0100

    Update server with ReadBuffer command

--- Aaru.Server/Components/Pages/Report/View.razor.cs
+++ Aaru.Server/Components/Pages/Report/View.razor.cs
@@ -651,6 +651,9 @@ public partial class View
                 if(report.SCSI.ReadCapabilities.SupportsLiteOnReadRawDVD == true)
                     readCapabilitiesList.Add("Device supports reading RAW DVD data using Lite-On READ BUFFER command");
 
+                if(report.SCSI.ReadCapabilities.SupportsReadBuffer3CReadRawDVD == true)
+                    readCapabilitiesList.Add("Device supports reading RAW DVD data using READ BUFFER (3Ch) command");
+
                 if(readCapabilitiesList.Count       > 0) ReadCapabilitiesList       = readCapabilitiesList;
                 if(readCapabilitiesDictionary.Count > 0) ReadCapabilitiesDictionary = readCapabilitiesDictionary;
             }
